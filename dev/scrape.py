46806cbd4b21feef2b41794d4695820714688f29
Ankush Patel <patelankush.2012@gmail.com>
2017-10-10 13:43:16 -0400

    Adding City filtering

--- dev/scrape.py
+++ dev/scrape.py
@@ -9,4 +9,12 @@
 import wikipedia
 
 # Gets Place Name from https://en.wikipedia.org/wiki/List_of_municipalities_in_Tennessee
-cities = wikipedia.page(title="List_of_municipalities_in_Tennessee", pageid=None, auto_suggest=True, redirect=True, preload=False).links
+cities = wikipedia.page(title='List_of_municipalities_in_Tennessee', pageid=None, auto_suggest=True, redirect=True, preload=False).links
+# Filters out links that are not cities
+filteredCities = filter(lambda x: ', Tennessee' in x and 'County' not in x, cities)
+
+for x in filteredCities:
+    print(x)
+
+# # Grabs latitude and longitude data for each city
+# latlngData = for x in filteredCities
